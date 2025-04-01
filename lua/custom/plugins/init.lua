37e6c1048062764061f9de1aea7c977d1c7ad6af
Robear Mankaryous <rmankaryous12@gmail.com>
2025-04-01 00:10:23 -0700

    undotree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { -- Review undo history with a tree view
+    'mbbill/undotree',
+    keys = {
+      { '<leader>u', ':UndotreeToggle<CR>', desc = 'Toggle Undotree' },
+    },
+  },
+}
